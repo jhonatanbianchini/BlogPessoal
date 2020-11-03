8b64cbec90f9146ad2b75a97cd0b5aa7729b9041
jhonatanbianchini <jhonatanbianchini0@gmail.com>
2020-11-03 19:56:31 -0300

    consumoApi1 04/11

--- blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
+++ blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-lateral.component.css']
 })
 export class PerfilLateralComponent implements OnInit {
+  nome : String;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    this.nome = 'Jhonatan Bianchini'
   }
 
 }
